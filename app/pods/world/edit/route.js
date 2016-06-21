921db835a6731305f7c5495981d3effe0af9ab47
Jarrett Drouillard <jarrett@thestyl.us>
2016-06-20 22:38:34 -0400

    feat: worlds{,s} basic cruds

--- app/pods/world/edit/route.js
+++ app/pods/world/edit/route.js
@@ -1,4 +1,33 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  setupController( controller, model ) {
+    this._super( controller, model );
+
+    controller.set( 'editName', model.get( 'name' ) );
+  },
+
+  actions: {
+    cancelEditWorld() {
+      window.history.back();
+    },
+
+    destroyWorld() {
+      const m = this.currentModel;
+
+      m.destroyRecord().then( () => {
+        this.transitionTo( 'worlds' );
+      });
+    },
+
+    submitEditWorld() {
+      const m = this.currentModel;
+
+      m.set( 'name', this.get( 'controller.editName' ) );
+
+      m.save().then( response => {
+        this.transitionTo( 'world', response );
+      });
+    }
+  }
 });
--- app/pods/worlds/new/route.js
+++ app/pods/worlds/new/route.js
@@ -1,4 +1,24 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return this.store.createRecord( 'world' );
+  },
+
+  actions: {
+    cancelNewWorld() {
+      this.transitionTo( 'worlds' );
+    },
+
+    submitNewWorld() {
+      const world = this.currentModel;
+
+      world.set( 'name', `${ this.get( 'controller.name' ) }` );
+
+      // Time to save the world! haha :)
+      world.save().then( response => {
+        this.transitionTo( 'world', response );
+      });
+    }
+  }
 });

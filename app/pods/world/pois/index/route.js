f74168fcdbe9d51fd9465957f1f94fd10184b4a1
Jarrett Drouillard <jarrett@thestyl.us>
2016-06-24 22:21:19 -0400

    chore: general cleanup

--- app/pods/world/pois/index/route.js
+++ app/pods/world/pois/index/route.js
@@ -4,5 +4,11 @@ export default Ember.Route.extend({
   model() {
     return this.modelFor( 'world' ).get( 'pois' );
     // return this.store.query( 'poi', { world: this.modelFor( 'world' ).get( 'id' ) });
+  },
+
+  setupController( controller, model ) {
+    this._super( controller, model );
+
+    controller.set( 'world', this.modelFor( 'world' ) );
   }
 });
